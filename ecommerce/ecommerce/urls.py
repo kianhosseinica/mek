928d7f04c2783cbd2da7718f485c2c4eea5acd09
kianhosseinica <169084644+kianhosseinica@users.noreply.github.com>
2025-05-27 21:54:16 -0400

    Update urls.py

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -15,10 +15,4 @@ urlpatterns = [
 
 ]
 
-# Custom 404 error handler
-handler404 = error_404
-
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
